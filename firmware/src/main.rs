09d48c4e73bab20301e8f50cab42ea460d8ecf98
Nathan Howell <nathan.d.howell@gmail.com>
2025-10-15 17:59:59 -0700

    stubbing

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+
+extern crate panic_halt;
+
+use embassy_executor::Spawner;
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
 }
